87147a3704005ab43191a896f963a637cebb7cf1
SheriefBadran <sb222rf@student.lnu.se>
2015-11-03 17:17:24 +0100

    Part of typescript setup.

---
+++ tsconfig.js
@@ -0,0 +1,20 @@
+{
+    "compilerOptions": {
+        "jsx": "preserve",
+        "inlineSourceMap": true,
+        "inlineSources": true,
+        "target": "es6",
+        "outDir": ".tmp/ts"
+    },
+    "filesGlob": [
+        "*.ts",
+        "app/**/*.tsx",
+        "app/**/*.ts",
+        "typings/*.ts"
+    ],
+    "files": [
+        "app/index.tsx",
+        "typings/es7.d.ts",
+        "typings/tsd.d.ts"
+    ]
+}
